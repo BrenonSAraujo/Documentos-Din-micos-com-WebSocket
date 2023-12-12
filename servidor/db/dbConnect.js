ab66cb8369ab5101fd73791ac4f7f8147bbb56b1
Brenon Silvestre <brenonsilvestre64@gmail.com>
2023-12-12 05:54:21 -0300

    Meu primerio commit

---
+++ servidor/db/dbConnect.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+
+const cliente = new MongoClient( process.env.DB_CONNECT_STRING );
+
+let documentosColecao, usuariosColecao;
+
+try {
+  await cliente.connect();
+
+  const db = cliente.db("alura-websockets");
+  documentosColecao = db.collection("documentos");
+  usuariosColecao = db.collection("usuarios");
+
+  console.log("Conectado ao banco de dados com sucesso!");
+} catch (erro) {
+  console.log(erro);
+}
+
+export { documentosColecao, usuariosColecao };
